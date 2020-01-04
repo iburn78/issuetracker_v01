0c0b3e752453f2e05e53fe02bbd12a023463173d
iburn78 <andy.chiwon.kim@gmail.com>
2020-01-04 17:16:45 +0900

    wip

--- blog/views.py
+++ blog/views.py
@@ -11,12 +11,6 @@ from django.views.generic import (
 from django.http import HttpResponse
 from .models import Post 
 
-def home(request):
-    context = {
-            'posts':Post.objects.all(),
-            }
-    return render(request, 'blog/home.html', context)
-
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
